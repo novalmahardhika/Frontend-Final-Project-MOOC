920ca62b1577f40ef609b58043ed92d4bc96bad9
novalmahardhika <asubasa9@gmail.com>
2023-11-22 00:57:32 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <div className='h-screen w-full flex justify-center items-center'>
+        <Button className=' flex justify-center items-center'>
+          Hello World
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default App
