dd71403094b7c46dd7195e5f63da29fc47a24f69
Dilla16 <fadillahanggraini16@gmail.com>
2023-12-11 22:04:40 +0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,86 +11,123 @@ import Card_Course from "./pages/course_list/card_course";
 import CourseDetail from "./pages/course_detail/detail_course";
 import LayoutBeranda from "./pages/beranda/layout";
 import UserProtected from "./pages/authentication/protected";
-import { CategoryProvider } from "./pages/course_list/categoryContext";
+import { CategoryProvider } from "./pages/course_list/categoryProvider";
+import LayoutNotification from "@/pages/notification/layout";
+import NotificationList from "./pages/notification/notificationPage";
+import { DialogDemo } from "./pages/course_detail/modal";
+import Payment from "./pages/payment/payment";
+import LayoutPayment from "@/pages/payment/layout";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route
-          path="/Admin"
-          element={
-            <LayoutAdmin>
-              <Dashboard />
-            </LayoutAdmin>
-          }
-        />
-        <Route
-          path="/AdminKelolaKelas"
-          element={
-            <LayoutAdmin>
-              <KelolaKelas />
-            </LayoutAdmin>
-          }
-        />
-        <Route
-          path="/User/Register"
-          element={
-            <LayoutUser>
-              <Register />
-            </LayoutUser>
-          }
-        />
-        <Route
-          path="/User/Login"
-          element={
-            <LayoutUser>
-              <UserLogin />
-            </LayoutUser>
-          }
-        />
-        <Route
-          path="/Admin/Login"
-          element={
-            <LayoutUser>
-              <AdminLogin />
-            </LayoutUser>
-          }
-        />
+      <CategoryProvider>
+        <Routes>
+          <Route
+            path="/Admin"
+            element={
+              <LayoutAdmin>
+                <Dashboard />
+              </LayoutAdmin>
+            }
+          />
+          <Route
+            path="/AdminKelolaKelas"
+            element={
+              <LayoutAdmin>
+                <KelolaKelas />
+              </LayoutAdmin>
+            }
+          />
+          <Route
+            path="/User/Register"
+            element={
+              <LayoutUser>
+                <Register />
+              </LayoutUser>
+            }
+          />
+          <Route
+            path="/User/Login"
+            element={
+              <LayoutUser>
+                <UserLogin />
+              </LayoutUser>
+            }
+          />
+          <Route
+            path="/Admin/Login"
+            element={
+              <LayoutUser>
+                <AdminLogin />
+              </LayoutUser>
+            }
+          />
 
-        <Route
-          path="/Beranda"
-          element={
-            <UserProtected>
+          <Route
+            path="/Beranda"
+            element={
               <LayoutBeranda>
                 <Beranda />
               </LayoutBeranda>
-            </UserProtected>
-          }
-        />
-        <Route
-          path="/Course"
-          element={
-            <UserProtected>
-              <CategoryProvider>
+            }
+          />
+          <Route
+            path="/Course"
+            element={
+              <UserProtected>
+                {/* <CategoryProvider> */}
                 <LayoutBeranda>
                   <Card_Course />
                 </LayoutBeranda>
-              </CategoryProvider>
-            </UserProtected>
-          }
-        />
-        <Route
-          path="/CourseDetail/:id"
-          element={
-            <UserProtected>
+                {/* </CategoryProvider> */}
+              </UserProtected>
+            }
+          />
+          <Route
+            path="/CourseDetail/:id"
+            element={
+              <UserProtected>
+                {/* <CategoryProvider> */}
+                <LayoutBeranda>
+                  <CourseDetail />
+                </LayoutBeranda>
+                {/* </CategoryProvider> */}
+              </UserProtected>
+            }
+          />
+
+          <Route
+            path="/notification"
+            element={
               <LayoutBeranda>
-                <CourseDetail />
+                <LayoutNotification>
+                  <NotificationList />
+                </LayoutNotification>
               </LayoutBeranda>
-            </UserProtected>
-          }
-        />
-      </Routes>
+            }
+          />
+
+          <Route
+            path="/dialogdemo"
+            element={
+              <LayoutBeranda>
+                <DialogDemo />
+              </LayoutBeranda>
+            }
+          />
+          <Route
+            path="/payment"
+            element={
+              <LayoutBeranda>
+                <LayoutPayment>
+                  <Payment />
+                </LayoutPayment>
+              </LayoutBeranda>
+            }
+          />
+        </Routes>
+      </CategoryProvider>
     </Router>
   );
 }
