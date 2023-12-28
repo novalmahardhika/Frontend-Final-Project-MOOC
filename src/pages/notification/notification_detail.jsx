0fa7484e40e1a05a7be115b5c9fa5846d93a8fbd
novalmahardhika <asubasa9@gmail.com>
2023-12-28 15:51:35 +0700

    feat: fixed path directory

--- src/pages/notification/notification_detail.jsx
+++ src/pages/notification/notification_detail.jsx
@@ -1,23 +1,25 @@
-import Notification from "./list";
-import Detail from "./Detail";
+import Notification from './list'
+import Detail from './detail'
 
 const NotificationDetail = () => {
   return (
-    <div className="relative z-1 font-poppins">
-      <div className="bg-secondary">
-        <div className="container pt-12 pb-20">
-          <div className=" text-3xl font-semibold text-primary">Notification </div>
+    <div className='relative z-1 font-poppins'>
+      <div className='bg-secondary'>
+        <div className='container pt-12 pb-20'>
+          <div className='text-3xl font-semibold text-primary'>
+            Notification{' '}
+          </div>
         </div>
       </div>
-      <div className="container flex gap-10 justify-between">
-        <div className="w-[500px]">
+      <div className='container flex justify-between gap-10'>
+        <div className='w-[500px]'>
           <Notification />
         </div>
-        <div className=" w-[900px]">
+        <div className=' w-[900px]'>
           <Detail />
         </div>
       </div>
     </div>
-  );
-};
-export default NotificationDetail;
+  )
+}
+export default NotificationDetail
