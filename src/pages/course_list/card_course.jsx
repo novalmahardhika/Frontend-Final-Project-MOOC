4a427e3139114919a3b2824f8f7b1cfc10be9173
Dilla16 <fadillahanggraini16@gmail.com>
2023-12-29 01:47:39 +0700

    responsive course list

--- src/pages/course_list/card_course.jsx
+++ src/pages/course_list/card_course.jsx
@@ -1,16 +1,38 @@
 // Card_Course.js
-import { Card, CardTitle } from "@/components/ui/card";
-import StarRating from "./StarRating";
+import { Card } from "@/components/ui/card";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import LevelBars from "@/pages/course_list/level_bars";
 import PropTypes from "prop-types";
-
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMedal, faClock, faBook, faStar, faGem, faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+// eslint-disable-next-line react-refresh/only-export-components
+const LoadingSkeletonCard = () => (
+  <Card className="md:w-[420px] w-[300px] h-[280px] md:h-full pb-5">
+    <div className="hover:opacity-50 cursor-pointer hover:transition-transform space-y-4">
+      <Skeleton className=" object-cover w-full h-32 md:h-48 rounded-t-sm rounded-b-none" />
+      <div className="space-y-3 p-3">
+        <div className="flex justify-between">
+          <Skeleton className=" w-32 md:w-52 h-2" />
+          <Skeleton className="w-10 h-2" />
+        </div>
+        <Skeleton className=" w-20 md:w-32 h-2" />
+        <div className="flex flex-wrap justify-between gap-5">
+          <Skeleton className=" w-16 md:w-20 h-2" />
+          <Skeleton className=" w-16 md:w-20 h-2" />
+          <Skeleton className=" w-16 md:w-20 h-2" />
+        </div>
+      </div>
+    </div>
+  </Card>
+);
 
 const Card_Course = ({ selectedFilters }) => {
+  const [loading, setLoading] = useState(true);
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const categoryParam = searchParams.get("category");
@@ -23,8 +45,10 @@ const Card_Course = ({ selectedFilters }) => {
         let url = "https://idea-academy.up.railway.app/api/v1/courses";
         const res = await axios.get(url);
         setCourseList(res.data.data);
+        setLoading(false);
       } catch (err) {
         console.log(err);
+        setLoading(false);
       }
     };
 
@@ -37,7 +61,7 @@ const Card_Course = ({ selectedFilters }) => {
     });
 
     if (selectedSort === "Terbaru") {
-      filteredCourses = filteredCourses.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+      filteredCourses = filteredCourses.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
     } else if (selectedSort === "Terpopuler") {
       filteredCourses = filteredCourses.sort((a, b) => b.rating - a.rating);
     } else if (selectedSort === "Promo") {
@@ -49,9 +73,30 @@ const Card_Course = ({ selectedFilters }) => {
     return filteredCourses;
   };
 
-  const formatCurrency = (amount) => {
-    return new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(amount);
-  };
+  if (loading) {
+    return (
+      <div className="container mt-3">
+        <div className="flex flex-wrap gap-6">
+          {[...Array(2)].map((_, index) => (
+            <LoadingSkeletonCard key={index} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  // const formatCurrency = (course) => {
+  //   if (course.type === "free") {
+  //     return "Free";
+  //   } else {
+  //     return new Intl.NumberFormat("id-ID", {
+  //       style: "currency",
+  //       currency: "IDR",
+  //       minimumFractionDigits: 0,
+  //       maximumFractionDigits: 0,
+  //     }).format(course.price);
+  //   }
+  // };
 
   if (filterCourses().length === 0) {
     return <div className="flex text-center mx-auto text-xl font-semibold ">No courses available</div>;
@@ -59,49 +104,74 @@ const Card_Course = ({ selectedFilters }) => {
 
   return (
     <div className=" font-poppins">
-      <div className="font-poppins flex flex-wrap gap-10 mb-20 w-full">
+      <div className="font-poppins flex flex-wrap gap-10 md:mb-20 w-full justify-center md:justify-start">
         {filterCourses().map((course) => (
-          <Card
-            key={course.id}
-            className="md:w-[400px] h-[360px]"
+          <div
+            className="space-y-3"
+            key={course.title}
           >
-            <div className=" hover:opacity-50 cursor-pointer hover:transition-transform">
-              <img
-                className="object-cover w-full h-48 rounded-t-sm"
-                src={course.image}
-                alt={course.title}
-              />
-            </div>
-
-            <div className="mx-5 space-y-5">
-              <div className="space-y-3">
-                <div className="space-y-1">
-                  <Link to={`/course/${course.id}`}>
-                    <CardTitle className="text-[18px] font-semibold pt-2">{course.title}</CardTitle>
-                  </Link>
-                  <div className="text-xs">#{course.category}</div>
+            <Card className="md:w-[420px] w-[300px] h-full md:h-full">
+              <Link to={`/Course/${course.id}`}>
+                <div className="hover:opacity-50 cursor-pointer hover:transition-transform">
+                  <img
+                    className="object-cover w-full h-32 md:h-48 rounded-t-sm"
+                    src={course.image}
+                    alt={course.title}
+                  />
+                </div>
+              </Link>
+              <div className="p-4 space-y-1">
+                <div className="flex justify-between items-center">
+                  <div className="text-xs font-semibold text-active ">{course.category}</div>
+                  <div className="flex space-x-2">
+                    <FontAwesomeIcon
+                      icon={faStar}
+                      className="text-active text-sm ms:text-lg"
+                    />
+                    <div className="text-xs md:text-sm">{course.rating}</div>
+                  </div>
+                </div>
+                <Link to={`/Course/${course.id}`}>
+                  <div className="text-sm text-primary font-semibold">{course.title}</div>
+                </Link>
+                <div className="text-xs">by {course.creator}</div>
+                <div className="flex flex-wrap gap-3 justify-between items-center pt-2 pb-3">
+                  <div className="flex items-center space-x-2">
+                    <FontAwesomeIcon
+                      icon={faMedal}
+                      className="text-success"
+                    />
+                    <div className="text-xs">{course.level === "beginner" ? "Beginner" : course.level === "intermediate" ? "Intermediate" : "Advance"}</div>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <FontAwesomeIcon
+                      icon={faBook}
+                      className="text-success"
+                    />
+                    <div className="text-xs">{course.totalModule} Modules</div>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <FontAwesomeIcon
+                      icon={faClock}
+                      className="text-success"
+                    />
+                    <div className="text-xs">{course.totalDuration} Menit</div>
+                  </div>
+                </div>
+                <div>
+                  {course.type === "Free" ? (
+                    <Button className="h-7 text-xs flex gap-3 bg-active text-white">
+                      <FontAwesomeIcon icon={faGem} /> Premium{" "}
+                    </Button>
+                  ) : (
+                    <Button className="h-7 text-xs bg-primary flex gap-3 ">
+                      <FontAwesomeIcon icon={faCirclePlay} /> Mulai Kelas{" "}
+                    </Button>
+                  )}
                 </div>
               </div>
-
-              <div>
-                <div>{formatCurrency(course.price)}</div>
-              </div>
-              <div className="flex justify-between">
-                <div className="flex items-center gap-2">
-                  <StarRating rating={course.rating} />
-                  <span className="text-xs">({course.rating})</span>
-                </div>
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <div className="text-xs text-primary">{course.level === "beginner" ? <LevelBars count={1} /> : course.level === "intermediate" ? <LevelBars count={2} /> : <LevelBars count={3} />}</div>
-                    </TooltipTrigger>
-                    <TooltipContent className=" bg-white shadow-md text-primary"> {course.level === "beginner" ? "Beginner" : course.level === "intermediate" ? "Intermediate" : "Advance"}</TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              </div>
-            </div>
-          </Card>
+            </Card>
+          </div>
         ))}
       </div>
     </div>
