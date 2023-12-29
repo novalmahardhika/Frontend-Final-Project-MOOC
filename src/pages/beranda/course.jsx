a6fda4382e0fb9b6c2a3f26c08afe2d09c2d80e5
Dilla16 <fadillahanggraini16@gmail.com>
2023-12-29 12:36:26 +0700

    update responsive

--- src/pages/beranda/course.jsx
+++ src/pages/beranda/course.jsx
@@ -130,7 +130,7 @@ const Course = ({ activeTab }) => {
                 </div>
                 <div>
                   <Link to={`/Course/${item.id}`}>
-                    {item.type === "Free" ? (
+                    {item.type === "free" ? (
                       <Button className="h-7 text-xs flex gap-3 bg-active text-white">
                         <FontAwesomeIcon icon={faGem} /> Premium{" "}
                       </Button>
