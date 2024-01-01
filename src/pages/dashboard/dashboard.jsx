3b22d3e89fc7b8c6573669afc2a28a6c24b55a5b
Dilla16 <fadillahanggraini16@gmail.com>
2024-01-01 23:48:10 +0700

    update table payments

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,77 +1,138 @@
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useState, useEffect } from "react";
-import Filter from "@/components/filter_button";
 import axios from "axios";
+import Search from "@/components/search_table";
+
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationPrevious, PaginationNext } from "@/components/ui/pagination"; // Sesuaikan dengan komponen paginasi yang Anda gunakan
 
 const Dashboard = () => {
   const [payments, setPayments] = useState([]);
-  const [filterType, setFilterType] = useState("DESC");
-
-  async function getData() {
-    try {
-      const data = await axios.get("https://idea-academy.up.railway.app/api/v1/orders/list", {
-        headers: {
-          Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImMwNDA5ZmI0LTNlMWUtNDFhNC04NzI2LTQxYTBjMmE2ZTk5ZSIsImlhdCI6MTcwMzMwNzU4M30.CMDb7Xw1730zLb-0PVuHR4L0YimuH-iABs3BbPfeYVw"
-        }
-      });
-      setPayments(data.data.data);
-      return data.data.data;
-    } catch (err) {
-      console.error(err)
-    }
-  }
+  const [userNames, setUserNames] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
+  const itemsPerPage = 10;
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
-    getData();
-  }, [])
+    const fetchCourse = async () => {
+      try {
+        const res = await axios.get(`https://idea-academy.up.railway.app/api/v1/orders/list`, { headers: { Authorization: `Bearer ${token}` } });
+        setPayments(res.data.data);
+        const userIds = res.data.data.map((item) => item.userId);
+        const uniqueUserIds = [...new Set(userIds)];
+        const userNamesMap = {};
+        for (const userId of uniqueUserIds) {
+          const resUser = await axios.get(`https://idea-academy.up.railway.app/api/v1/users?id=${userId}`, {
+            headers: { Authorization: `Bearer ${token}` },
+          });
+          const userData = resUser.data.data[0];
+          if (userData) {
+            userNamesMap[userId] = userData.name;
+          }
+        }
+        setUserNames(userNamesMap);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchCourse();
+  }, [token]);
 
-  const handleFilterChange = (newFilterType) => {
-    setFilterType(newFilterType);
-  };
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
 
-  // Sorting the dummyPayments based on the filter type
-  const sortedPayments = payments.sort((a, b) => {
-    if (filterType === "ASC") {
-      return a.id.localeCompare(b.id);
-    } else {
-      return b.id.localeCompare(a.id);
-    }
+  const filteredPayments = payments.filter((item) => {
+    const courseTitle = item.Course?.title || ""; // Check if item.Course is null or undefined
+    const status = item.status || "";
+    const paymentMethod = item.paymentMethod || "";
+
+    return courseTitle.toLowerCase().includes(searchTerm.toLowerCase()) || status.toLowerCase().includes(searchTerm.toLowerCase()) || paymentMethod.toLowerCase().includes(searchTerm.toLowerCase());
   });
 
+  const currentItems = filteredPayments.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalItemsAfterSearch = filteredPayments.length;
+  const totalPaginationPages = Math.ceil(totalItemsAfterSearch / itemsPerPage);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <body className="px-10 font-poppins">
-      <div className="flex flex-1 items-center justify-between mb-4">
+    <div className="px-10 font-poppins">
+      <div className="mt-2 flex items-center justify-between mb-4">
         <div className="text-2xl font-semibold"> Status Pembayaran</div>
-        <Filter onFilterChange={handleFilterChange} />
+        <div className="flex space-x-5 items-center">
+          <Search onSearchChange={(term) => setSearchTerm(term)} />
+        </div>
       </div>
-      <div>
-        <Table>
-          <TableCaption>Tabel Status Pembayaran.</TableCaption>
-          <TableHeader className="bg-secondary h-12">
-            <TableRow>
-              <TableHead className="text-primary font-bold">ID</TableHead>
-              <TableHead className="text-primary font-bold">Kategori</TableHead>
-              <TableHead className="text-primary font-bold">Kelas Premium</TableHead>
-              <TableHead className="text-primary font-bold">Status</TableHead>
-              <TableHead className="text-primary font-bold">Metode Pembayaran</TableHead>
-              <TableHead className="text-primary font-bold">Tanggal Bayar</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {sortedPayments.map((item) => (
-              <TableRow key={item.id}>
-                <TableCell className="font-medium">{item.userId}</TableCell>
-                <TableCell>{item.Course.category}</TableCell>
-                <TableCell>{item.Course.title}</TableCell>
-                <TableCell className={item.status === "PENDING" ? "text-destructive font-medium" : "text-success font-medium"}>{item.status}</TableCell>
-                <TableCell>{item.paymentMethod}</TableCell>
-                <TableCell>{(item.status === "COMPLETED") ? item.updatedAt : ""}</TableCell>
+      <div className="mb-3">
+        {totalItemsAfterSearch === 0 ? (
+          <div className="text-primary text-center">Tidak ditemukan data seperti yang Anda cari.</div>
+        ) : (
+          <Table>
+            <TableHeader className="bg-secondary h-12">
+              <TableRow>
+                <TableHead className="text-primary font-bold">ID</TableHead>
+                <TableHead className="text-primary font-bold">Kategori</TableHead>
+                <TableHead className="text-primary font-bold">Kelas Premium</TableHead>
+                <TableHead className="text-primary font-bold">Status</TableHead>
+                <TableHead className="text-primary font-bold">Metode Pembayaran</TableHead>
+                <TableHead className="text-primary font-bold">Tanggal Bayar</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody className="">
+              {currentItems.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell className="font-medium">{userNames[item.userId]}</TableCell>
+                  <TableCell>{item.Course.category}</TableCell>
+                  <TableCell>{item.Course.title}</TableCell>
+                  <TableCell className={item.status === "PENDING" ? "text-destructive font-medium" : "text-success font-medium"}>{item.status}</TableCell>
+                  <TableCell>{item.paymentMethod}</TableCell>
+                  <TableCell>{new Date(item.updatedAt).toLocaleDateString("id-ID", { year: "numeric", month: "short", day: "numeric" })}</TableCell>{" "}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </div>
-    </body>
+      <Pagination>
+        <PaginationContent className="cursor-pointer">
+          <PaginationItem>
+            {currentPage === 1 ? (
+              <PaginationPrevious
+                disabled
+                className="cursor-not-allowed"
+              />
+            ) : (
+              <PaginationPrevious
+                onClick={() => {
+                  console.log(currentPage);
+                  paginate(currentPage - 1);
+                }}
+                disabled={currentPage === 1}
+              />
+            )}
+          </PaginationItem>
+          {[...Array(totalPaginationPages).keys()].map((number) => (
+            <PaginationItem
+              key={number + 1}
+              className={currentPage === number + 1 ? "bg-primary text-white rounded-lg cursor-not-allowed" : ""}
+            >
+              <PaginationLink onClick={() => paginate(number + 1)}>{number + 1}</PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem>
+            {currentPage === totalPaginationPages || totalItemsAfterSearch === 0 ? (
+              <PaginationNext
+                disabled
+                className="cursor-not-allowed"
+              />
+            ) : (
+              <PaginationNext onClick={() => paginate(currentPage + 1)} />
+            )}
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
   );
 };
 
